6bb4dd35b7c67a0477bfa45af3565dba4008d243
ysh3718 <yof3718@naver.com>
2019-10-16 02:21:43 +0900

    MEMODAO EDIT용 1건 조회 메서드 등록

--- MEMO/src/MEMO/MEMODAO.java
+++ MEMO/src/MEMO/MEMODAO.java
@@ -95,7 +95,7 @@ public class MEMODAO {
 		
 		ArrayList<MEMODTO> MEMOList = new ArrayList<MEMODTO>();
 		
-		String sql = "select id, title, content, memoDate from Memo";
+		String sql = "select id, title, content, memoDate from MEMO";
 
 		try {
 			
@@ -128,5 +128,45 @@ public class MEMODAO {
 		return MEMOList;
 	}
 
+	/******************************************************************************************/
+	// edit용 메모장 1건 조회 메서드
+	/******************************************************************************************/
+	public MEMODTO getDB(int id) {
+		
+		connect();
+		
+		MEMODTO MEMODTO = new MEMODTO();
+		
+		String sql = "select * from MEMO where id = ?";
+		
+		try {
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			// SQL문에 조회조건 입력
+			pstmt.setInt(1,id);
 
+			//SQL문 실행
+			ResultSet rs = pstmt.executeQuery();
+
+			// 데이터가 하나만 있으므로 rs.next()를 한번만 실행 한다.
+			rs.next();
+			
+			// DB Select결과를 DO 객체에 저장
+			MEMODTO.setId(rs.getInt("id"));
+			MEMODTO.setTitle(rs.getString("title"));
+			MEMODTO.setContent(rs.getString("content"));
+			MEMODTO.setMemoDate(rs.getString("memoDate"));
+			
+			rs.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			disconnect();
+		}
+		return MEMODTO;
+	}
+	
 }
