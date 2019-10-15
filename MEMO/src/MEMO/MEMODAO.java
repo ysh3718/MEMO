42041a32033c9adb1f2f54a0e95c6460c40e111e
ysh3718 <yof3718@naver.com>
2019-10-16 02:12:53 +0900

    MEMODAO MySQL연결정보 등록

--- MEMO/src/MEMO/MEMODAO.java
+++ MEMO/src/MEMO/MEMODAO.java
@@ -9,5 +9,14 @@ import MEMO.MEMODTO;
 // DAO(Data Access Object)
 public class MEMODAO {
 
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+
+	/* MySQL 연결정보 */
+	String jdbc_driver = "com.mysql.jdbc.Driver";
+	
+	String jdbc_url = "jdbc:mysql://127.0.0.1/jspdb?useSSL=true&verifyServerCertificate=false&serverTimezone=UTC";
+
+	
 
 }
