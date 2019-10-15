9e41811b1085eb706b7f64a1a9bf758174f15001
ysh3718 <yof3718@naver.com>
2019-10-16 02:09:15 +0900

    MEMODTO 필드 정의

--- MEMO/src/MEMO/MEMODTO.java
+++ MEMO/src/MEMO/MEMODTO.java
@@ -2,4 +2,35 @@ package MEMO;
 
 public class MEMODTO {
 
+	// set, get에서 사용하는 필드 정의
+	private int id;
+	private String title;
+	private String content;
+	private String memoDate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getMemoDate() {
+		return memoDate;
+	}
+	public void setMemoDate(String memoDate) {
+		this.memoDate = memoDate;
+	}
+	
 }
