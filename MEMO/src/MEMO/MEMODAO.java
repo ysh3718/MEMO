e292eb032b98cfa54cb44f267a7659a870143fc1
ysh3718 <yof3718@naver.com>
2019-10-16 02:11:51 +0900

    MEMODAO.java 파일 업로드

---
+++ MEMO/src/MEMO/MEMODAO.java
@@ -0,0 +1,13 @@
+package MEMO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import MEMO.MEMODTO;
+
+// 전달 데이터단위인 (DTO : Data Transfer Object)를 사용하면서 DB 데이터를 직접 처리하는
+// DAO(Data Access Object)
+public class MEMODAO {
+
+
+}
